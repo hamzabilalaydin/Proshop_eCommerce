43fa8e69e65f80c6fc6817b374bf09d0efbe021d
hamzabilalaydin <hamzaba61@gmail.com>
2024-03-17 18:36:30 +0300

    logout functionality added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import {
   Badge,
   Navbar,
@@ -7,15 +8,28 @@ import {
 } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/userApiSlice";
+import { logout } from "../slices/authSlice";
 import logo from "../assets/logo.png";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
   const { userInfo } = useSelector((state) => state.auth);
 
-  const logoutHandler = () => {
-    console.log("Logout");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
