116d5da762bed58efdb19adaeddb5975e3b0d27f
hamzabilalaydin <hamzaba61@gmail.com>
2024-03-31 15:15:53 +0300

    small bug fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,6 +13,7 @@ import { useLogoutMutation } from "../slices/userApiSlice";
 import { logout } from "../slices/authSlice";
 import SearchBox from "./SearchBox";
 import logo from "../assets/logo.png";
+import { resetCart } from "../slices/cartSlice";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -27,6 +28,7 @@ const Header = () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
+      dispatch(resetCart());
       navigate("/");
     } catch (err) {
       console.log(err);
--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -16,9 +16,7 @@ const authSlice = createSlice({
     },
     logout: (state, action) => {
       state.userInfo = null;
-      localStorage.removeItem("userInfo");
-      localStorage.removeItem("cart");
-      localStorage.removeItem("__paypal_storage__");
+      localStorage.clear();
     },
   },
 });
